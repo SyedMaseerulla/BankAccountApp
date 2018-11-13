28de8c519a001ea54686eabf5dfda18da01e150b
Your Name <you@example.com>
2018-11-13 21:10:59 +0530

    List of objects is implemented to access each persons data from csv file and generate their respective account details.

--- BankAccountApp.java
+++ BankAccountApp.java
@@ -1,30 +1,40 @@
 package bankAcc.App;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
 
 	public static void main(String[] args) {
-
-		/*Checking chk1 = new Checking("Tim Cook","25467852", 1500);
-		Savings sav1 = new Savings("James Brown","587231698", 2400);
-		sav1.compound();
-		chk1.showInfo();
-		System.out.println("******************************************************");
-		sav1.showInfo();
-		sav1.deposit(5000);
-		sav1.withdraw(4000);
-		sav1.transfer("Vodafone pvt.ltd.", 300);*/
+		
+		List <Account> accounts = new LinkedList <Account>(); //creating list of objects
 		
 		//read a csv file then decide the acc to create based on the data
+		
 		String file = "F:\\Documents\\JavaProject\\NewBankAccounts.csv";
-		List<String[]> NewAccountHolders = utilities.CSV.read(file);
+		List<String[]> NewAccountHolders = utilities.CSV.read(file); //calling the csv file from utilities package to read the csv file
+		
 		for (String[] accountHolder : NewAccountHolders) {
-			System.out.println("NEW ACCOUNT ");
-			System.out.println(accountHolder[0]);
-			System.out.println(accountHolder[1]);
-			System.out.println(accountHolder[2]);
-			System.out.println(accountHolder[3]);
+			String name = accountHolder[0];
+			String ssn = accountHolder[1];
+			String accountType = accountHolder[2];
+			double initDeposit = Double.parseDouble(accountHolder[3]);
+	
+			if(accountType.equalsIgnoreCase("savings")){
+				//System.out.println(" OPEN A SAVINGS ACCOUNT");
+				accounts.add(new Savings(name, ssn, initDeposit));
+			}
+			else if(accountType.equalsIgnoreCase("checking")){
+				//System.out.println(" OPEN A CHECKING ACCOUNT");
+				accounts.add(new Checking(name, ssn, initDeposit));
+			}
+			else{
+				System.out.println("ERROR IN OPENING ACCOUNT");
+			}
+		}
+		for (Account acc : accounts) {
+			acc.showInfo();
+			System.out.println("\n************************************************");
 		}
 	}
  
